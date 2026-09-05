324d88f52d3a786ad62715e15c5c53ba04e68ec4
Odysseus <101147725+3dWonderGuy@users.noreply.github.com>
2026-09-05 14:40:47 -0500

    chore: untrack build files and ignore build directories

--- src/android-project/app/build/intermediates/cxx/Debug/1w1x4f2g/logs/arm64-v8a/build_command_targets.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-"C:\\Users\\odyss\\AppData\\Local\\Android\\Sdk\\cmake\\3.30.3\\bin\\ninja.exe" ^
-  -C ^
-  "C:\\b\\fn\\Debug\\1w1x4f2g\\arm64-v8a" ^
-  main ^
-  svg2png
--- src/android-project/app/build/intermediates/cxx/Debug/4t543f5t/logs/arm64-v8a/build_command_targets.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-"C:\\Users\\odyss\\AppData\\Local\\Android\\Sdk\\cmake\\3.30.3\\bin\\ninja.exe" ^
-  -C ^
-  "C:\\SoftwareDevelopment\\UADE-Universal-Application-Development-Enviroment\\UniversalFrameworkapplication\\src\\android-project\\app\\.cxx\\Debug\\4t543f5t\\arm64-v8a" ^
-  TestApp ^
-  svg2png
