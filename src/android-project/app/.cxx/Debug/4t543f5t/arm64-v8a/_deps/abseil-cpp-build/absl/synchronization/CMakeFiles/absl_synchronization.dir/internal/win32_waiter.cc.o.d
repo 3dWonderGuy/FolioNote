5bca8a5a85e9585e8a3cb88fcf0f9efdbb2a00f3
Odysseus <101147725+3dWonderGuy@users.noreply.github.com>
2026-09-09 23:31:41 -0500

    few chanages to anroid lib fix

--- src/android-project/app/.cxx/Debug/4t543f5t/arm64-v8a/_deps/abseil-cpp-build/absl/synchronization/CMakeFiles/absl_synchronization.dir/internal/win32_waiter.cc.o.d
+++
@@ -1,3 +0,0 @@
-_deps/abseil-cpp-build/absl/synchronization/CMakeFiles/absl_synchronization.dir/internal/win32_waiter.cc.o: \
-  C:/SoftwareDevelopment/UADE-Universal-Application-Development-Enviroment/UniversalFrameworkapplication/third_party/abseil-cpp/absl/synchronization/internal/win32_waiter.cc \
-  C:/SoftwareDevelopment/UADE-Universal-Application-Development-Enviroment/UniversalFrameworkapplication/third_party/abseil-cpp/absl/synchronization/internal/win32_waiter.h
